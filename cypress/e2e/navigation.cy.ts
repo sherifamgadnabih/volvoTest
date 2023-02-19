fa518a0379eaf99899a7acd75cbeb0148d3a9c3d
Sherif Amgad <sherif.amgad.nabih@gmail.com>
2023-02-19 12:07:27 +0100

    added unit testing and e2e testing

---
+++ cypress/e2e/navigation.cy.ts
@@ -0,0 +1,15 @@
+describe("Navigation", () => {
+  it("should navigate to learn and shop pages", () => {
+    cy.visit("/");
+
+    cy.get('a[href*="/learn/xc90-recharge"]').eq(0).click();
+
+    cy.url().should("include", "/learn");
+
+    cy.visit("/");
+
+    cy.get('a[href*="shop/xc90-recharge"]').eq(0).click();
+
+    cy.url().should("include", "/shop");
+  });
+});
---
+++ src/components/Filter.cy.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Filter from "./Filter";
+import { ConfigProvider, StyleProvider, ThemePicker } from "vcc-ui";
+
+describe("<Filter />", () => {
+  it("renders the correct options", () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(
+      <ConfigProvider config={{}}>
+        <StyleProvider>
+          <ThemePicker variant="light">
+            <Filter
+              carTypes={["sedan", "suv"]}
+              onValueChange={(value: string) => {}}
+            />
+          </ThemePicker>
+        </StyleProvider>
+      </ConfigProvider>
+    );
+    cy.get("option").eq(2).contains("sedan");
+    cy.get("option").eq(3).contains("suv");
+  });
+});
