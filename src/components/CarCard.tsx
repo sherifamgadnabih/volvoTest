5849090f7c25c6045c408134481a791b7dccc262
Sherif Amgad <sherif.amgad.nabih@gmail.com>
2023-02-20 16:08:18 +0100

    updated the did mount condition

--- src/components/CarCard.tsx
+++ src/components/CarCard.tsx
@@ -11,71 +11,68 @@ const CarCard = (props: CardProps) => {
   const [hasMounted, setHasMounted] = useState<boolean>(false);
   useEffect(() => setHasMounted(true), []); // make sure the component has mounted to avoid the hydration problem in react
   const { imageUrl, modelName, bodyType, id, modelType } = carItem;
-  return (
-    hasMounted && (
-      <Fragment>
+  return hasMounted ? (
+    <Fragment>
+      <View
+        padding={2}
+        marginBottom={2}
+        marginTop={2}
+        extend={{ textAlign: "start" }}
+        data-testid={`pcard-${id}`}
+      >
+        <Text
+          as="h4"
+          variant="kelly"
+          subStyle="emphasis"
+          extend={{ color: "#8c8c8c" }}
+        >
+          {bodyType}
+        </Text>
         <View
-          padding={2}
-          marginBottom={2}
-          marginTop={2}
-          extend={{ textAlign: "start" }}
-          data-testid={`pcard-${id}`}
+          direction={{ default: "column", fromL: "row" }}
+          alignItems={{ default: "start", fromM: "left" }}
         >
           <Text
-            as="h4"
-            variant="kelly"
+            as="h5"
+            variant="hillary"
             subStyle="emphasis"
-            extend={{ color: "#8c8c8c" }}
+            extend={{ marginRight: 10 }}
           >
-            {bodyType}
+            {modelName}
           </Text>
-          <View
-            direction={{ default: "column", fromL: "row" }}
-            alignItems={{ default: "start", fromM: "left" }}
-          >
-            <Text
-              as="h5"
-              variant="hillary"
-              subStyle="emphasis"
-              extend={{ marginRight: 10 }}
-            >
-              {modelName}
+          <Block extend={{ display: "block" }}>
+            <Text variant="kelly" extend={{ color: "#8c8c8c", padding: "2em" }}>
+              {modelType}
             </Text>
-            <Block extend={{ display: "block" }}>
-              <Text
-                variant="kelly"
-                extend={{ color: "#8c8c8c", padding: "2em" }}
-              >
-                {modelType}
-              </Text>
-            </Block>
-          </View>
-          <Spacer />
-          <Image
-            width={320}
-            height={240}
-            src={imageUrl}
-            layout="intrinsic"
-            priority
-            alt={`${modelName} - ${modelType}`}
-          />
-          {showLinks && (
-            <View direction="row" justifyContent="center">
-              <Link href={`/learn/${id}`} arrow="right">
-                Learn
-              </Link>
-              <Spacer />
-              <Link href={`/shop/${id}`} arrow="right">
-                Shop
-              </Link>
-            </View>
-          )}
+          </Block>
         </View>
+        <Spacer />
+        <Image
+          width={320}
+          height={240}
+          src={imageUrl}
+          layout="intrinsic"
+          priority
+          alt={`${modelName} - ${modelType}`}
+        />
+        {showLinks && (
+          <View direction="row" justifyContent="center">
+            <Link href={`/learn/${id}`} arrow="right">
+              Learn
+            </Link>
+            <Spacer />
+            <Link href={`/shop/${id}`} arrow="right">
+              Shop
+            </Link>
+          </View>
+        )}
+      </View>
 
-        <Spacer />
-        <Spacer />
-      </Fragment>
-    )
+      <Spacer />
+      <Spacer />
+    </Fragment>
+  ) : (
+    <></>
   );
 };
 export default CarCard;
